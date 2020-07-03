ce0265b83829f1373fbc0fc4a15c3216c07ee458
Kladmen228 <dakfadeev@gmail.com>
2020-07-04 01:26:31 +0300

    build

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.popup import Popup
 from kivy.config import Config
 from kivy.uix.label import Label
+import psycopg2
 import bcrypt
 
 Config.set('graphics', 'resizable', 1)
